4ee41c10df1b1554d52e8422de98a89ec0cf693b
Leon Hoogenraad <le.o.n@outlook.com>
2020-09-30 14:08:01 +1300

    Going to have to email David, no clue why resource is forbidden

--- upload.sh
+++ upload.sh
@@ -18,11 +18,3 @@ a2dissite 000-default
 
 # Reloading the webserver config to affect our changes
 service apache2 reload
-
-echo This machine is responsible for allowing a user to upload their past exam answers.
-echo It runs an apache2 webserver which serves a webpage on 127.0.0.1:8090/upload.php that provides
-echo a form to enter answer data into.
-
-echo This machine should now be running. If you also have the display VM running, there should be a
-echo link to navigate to the upload page from 127.0.0.1:8080/index.php
-echo If not, and you just want to enter some data, head to 127.0.0.1:8090/upload.php
