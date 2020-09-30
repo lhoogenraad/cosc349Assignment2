197998acc5d4b140d8e0f19b48bf7f7860b0ce90
Leon Hoogenraad <le.o.n@outlook.com>
2020-09-30 13:40:56 +1300

    Removed echo calls in display.sh, and rearranged chmod calls in display.sh to appear after copying the config file to the apache/sites-available folder

--- display.sh
+++ display.sh
@@ -1,6 +1,5 @@
 echo hello, world
 
-apt-get update
 
 # Installing the apache2, mysql, and php pakcages
 apt-get install -y apache2 php libapache2-mod-php php-mysql
@@ -10,6 +9,10 @@ apt-get install -y apache2 php libapache2-mod-php php-mysql
 # to use the one from the shared folder
 cp /vagrant/display.conf /etc/apache2/sites-available/
 
+chmod 777 /vagrant/www/display
+chmod 777 /vagrant/www/display/index.php
+chmod 777 /vagrant/www/display/index.css
+
 # Activating our website config
 a2ensite display
 
@@ -18,12 +21,4 @@ a2dissite 000-default
 
 # Reloading the webserver config to affect our changes
 service apache2 reload
-
-echo This machine runs a forwaded port on 127.0.0.1:8080, which accepts http requests using apache2 webserver
-echo This machine is also running an interface on 192.168.11, which
-echo allows it to access the mysql database server which runs on 192.168.2.12
-
-
-echo This machine should now be accepting http requests.
-echo navigate to: 127.0.0.1:8080/index.php to access the exam answers home page.
 
