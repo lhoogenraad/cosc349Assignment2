541a67eea43d3190b7444a4a5726cd828742402f
Leon Hoogenraad <le.o.n@outlook.com>
2020-10-01 12:48:10 +1300

    added chmod calls in upload.sh as well.

--- upload.sh
+++ upload.sh
@@ -5,6 +5,11 @@ apt-get update
 # Installing the apache2, mysql, and php pakcages
 apt-get install -y apache2 php libapache2-mod-php php-mysql
 
+chmod 755 /vagrant
+chmod 755 /vagrant/www
+chmod 755 /vagrant/www/upload
+chmod 755 /vagrant/www/upload/upload.css
+chmod 755 /vagrant/www/upload/upload.php
 
 # Here we change the VM's web server config
 # to use the one from the shared folder
